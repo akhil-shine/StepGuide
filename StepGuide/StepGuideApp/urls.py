4ca7d26f7da8ca2843211aa92b25c63bd4bdbc6d
Akhil Shine <akhilshine02@gmail.com>
2023-09-19 16:41:30 +0530

    commit

---
+++ StepGuide/StepGuideApp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('about/',views.about,name='about'),
+]
