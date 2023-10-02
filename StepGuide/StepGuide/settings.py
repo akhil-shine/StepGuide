1a6b8e9b7d22ed35fc672012d039b2efc7b30f63
Akhil Shine <akhilshine02@gmail.com>
2023-10-02 13:02:46 +0530

    Add forgot password

--- StepGuide/StepGuide/settings.py
+++ StepGuide/StepGuide/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',   
     'StepGuideApp'
+    
 ]
 
 MIDDLEWARE = [
@@ -137,7 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'akhilshine14@gmail.com'
-EMAIL_HOST_PASSWORD = 'jffcgxhptqtptuap'
-DEFAULT_FROM_EMAIL = 'akhilshine14@gmail.com'
+EMAIL_HOST_USER = 'stepguidee@gmail.com'
+EMAIL_HOST_PASSWORD = 'npkhumennrgscclf'
+DEFAULT_FROM_EMAIL = 'stepguidee@gmail.com'
 PASSWORD_RESET_TIMEOUT = 14400
+
